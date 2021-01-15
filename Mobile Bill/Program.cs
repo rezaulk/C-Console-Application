6a7f615e244f97788448a19fa3821b7960d83052
reza ul <rezaul.bd77@gmail.com>
2021-01-15 12:41:40 +0600

    Initial

---
+++ Mobile Bill/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobile_Bill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Enter a date: ");
+            DateTime Starttime;
+            bool datecheck = false;
+            if (DateTime.TryParse(Console.ReadLine(), out Starttime))
+            {
+                datecheck = true;
+            }
+            else
+            {
+                Console.WriteLine("You have entered an incorrect value.");
+            }
+
+            Console.WriteLine("Enter a date: ");
+            DateTime Endtime;
+            if (DateTime.TryParse(Console.ReadLine(), out Endtime))
+            {
+                datecheck = true;
+            }
+            else
+            {
+                Console.WriteLine("You have entered an incorrect value.");
+            }
+
+            if(datecheck == true)
+            {
+                var diff = System.Math.Abs((Endtime - Starttime).TotalSeconds);
+                var TotalAmount = 0;
+
+                Console.WriteLine("Breakdown: ");
+
+                while (diff > -1)
+                {
+                    DateTime BeforeChangedTime = Starttime;
+                    Starttime = Starttime.AddSeconds(20);
+                    int poisa = GetPulse(Starttime);
+                    Console.WriteLine(BeforeChangedTime + " + 20 second (" + Starttime + ") = " + poisa + " paisa");
+                    
+                    TotalAmount += poisa;
+                    diff -= 20;
+                }
+
+                double money = (double)TotalAmount / 100;
+                Console.WriteLine(money + " Taka");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("You have entered an incorrect value.");
+            }
+        }
+
+        static public int GetPulse(DateTime date)
+        {
+
+            int ActualTime = date.Hour;
+            if (ActualTime >= 9 && ActualTime <= 23)
+            {
+                return 30;
+            }
+            else
+            {
+                return 20;
+            }
+        }
+        
+    }
+}
